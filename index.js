2c6cdb2f81dc2e4a656e342b3407314cdeef0bf7
D <dsuchy@DS-MacBook-Pro.local>
2016-09-10 20:26:44 -0700

    Submit the latest changes.

--- index.js
+++ index.js
@@ -10,9 +10,11 @@
 
 module.exports = require('./lib/express');
 
-
 var express = require('express');
 var app = express();
+// var vcapServices = require('vcap_services');
+// var extend = require('util')._extend;
+var watson = require('watson-developer-cloud');
 
 app.get('/', function (req, res) {
   res.json({ 'hey': 'what up' });
@@ -21,6 +23,24 @@ app.get('/', function (req, res) {
 app.get('/make-call', function (req, res) {
   res.json({ 'hey': 'what up' });
 });
+
+app.get('/watson-token', function(req, res) {
+  var authorization = new watson.AuthorizationV1({
+    username: '89f3427c-033f-4b69-9209-e96c396a9e86',
+    password: 'QapGlIwXZtOx',
+    url: watson.TextToSpeechV1.URL
+  });
+
+  authorization.getToken(function (err, token) {
+    if (!token) {
+      console.log('error:', err);
+    } else {
+      res.json(token);
+      // Use your token here
+    }
+  });
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
